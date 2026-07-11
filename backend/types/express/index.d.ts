916295a66f29cf07ad90942aaeda23f68869be10
cstim240 <timsupann@gmail.com>
2026-07-10 22:40:52 -0700

    tighten req.user type, this define the JWT payload better

--- backend/types/express/index.d.ts
+++ backend/types/express/index.d.ts
@@ -3,8 +3,8 @@ import 'express';
 declare module 'express-serve-static-core' {
   interface Request {
     user?: {
-      role?: string;
-      [key: string]: unknown;
+      user_id: number
+      role: string;
     };
   }
 }
