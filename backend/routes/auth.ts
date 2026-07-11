4defe858ffc8a0cf47c896dae9ed736276a178db
cstim240 <timsupann@gmail.com>
2026-07-10 23:28:04 -0700

    create helper func for upcoming login/register routes and also had to install types for bcrpyt

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,4 +1,20 @@
-import { Router } from 'express';
+import { Router, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { pool } from '../db';
+import requireAuth from '../middleware/requireAuth';
+import type { JwtPayload } from '../types/jwtPayload';
+
+// (used by login and register route) ~helper function takes who the user is, turns it into tamper-proof signed token, tell browser to store the token safely, and keep sending it back automatically for the next 2 hours
+function setAuthCookie(res: Response, payload: JwtPayload): void {
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '2h'});
+    res.cookie('token', token, {
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 2 * 60 * 60 * 1000
+    });
+}
 
 const router = Router();
 
