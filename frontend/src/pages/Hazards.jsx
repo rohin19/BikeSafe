4e41710aaf6c55e53357386cbfe12469858d8f0a
cstim240 <timsupann@gmail.com>
2026-07-11 22:44:43 -0700

    temporary placeholders for each page

---
+++ frontend/src/pages/Hazards.jsx
@@ -0,0 +1,9 @@
+export default function Hazards() {
+  return (
+    <div className="page">
+      <h1>Hazards</h1>
+    </div>
+  )
+}
+
+
---
+++ frontend/src/pages/ReportHazard.jsx
@@ -0,0 +1,7 @@
+export default function ReportHazard() {
+  return (
+    <div className="page">
+      <h1>Report a Hazard</h1>
+    </div>
+  )
+}
