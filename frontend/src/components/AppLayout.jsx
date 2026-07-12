87d1d24f97d4c1b333702d05feeb6c75acf1fbff
cstim240 <timsupann@gmail.com>
2026-07-11 21:43:11 -0700

    smol edit

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import BottomNav from './BottomNav'
 
 // Outlet component is react router's placeholder for whatever child route matched
 // could be the components in the /pages directory (Home, Hazards, ReportHazards)
-function AppLayout() {
+export default function AppLayout() {
     return(
         <div className="website-frame">
             <div className="page-outlet">
