906a727da8f4fa69bfa676c16939d515a9c56f51
cstim240 <timsupann@gmail.com>
2026-07-12 17:12:36 -0700

    create AuthPanelProps interface to be used for component props

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -57,17 +57,22 @@ export interface HazardStats {
 }
 
 export interface Route {
-  route_id?: number
-  start_name: string
-  start_latitude: number
-  start_longitude: number
-  destination_name: string
-  destination_latitude: number
-  destination_longitude: number
-  elevation: number
-  distance: number
-  duration: number
-  safety_score: number
-  created_at?: string
-  created_by: number
+  route_id?: number;
+  start_name: string;
+  start_latitude: number;
+  start_longitude: number;
+  destination_name: string;
+  destination_latitude: number;
+  destination_longitude: number;
+  elevation: number;
+  distance: number;
+  duration: number;
+  safety_score: number;
+  created_at?: string;
+  created_by: number;
+}
+
+interface AuthPanelProps {
+  user: User | null;
+  onUserChange: (user: User | null) => void;
 }
