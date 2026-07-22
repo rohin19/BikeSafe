90eaaa27eacc892f214a5106ca002bf0ed438d5e
Yu <yud@Yus-MacBook-Neo.local>
2026-07-22 11:51:33 -0700

    insert map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export const Map = () => {
+export default function Map () {
   // Vancouver coordinates: [latitude, longitude]
   const vancouverCoords: [number, number] = [49.2827, -123.1207];
 
   return (
     <MapContainer 
       center={vancouverCoords} 
-      zoom={13} 
-      style={{ height: '100vh', width: '100%' }}
+      zoom={12} 
+      style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- frontend/src/pages/BikeSharePage.tsx
+++ frontend/src/pages/BikeSharePage.tsx
@@ -1,8 +1,12 @@
+import Map from "../components/Map";
+
 export default function BikeSharePage() {
     return (
         <div className="page">
             <h1>BikeShare</h1>
-            <p>Coming soon!</p>
+            <div className="map-placeholder">
+                <Map />
+            </div>
         </div>
     )
 }
